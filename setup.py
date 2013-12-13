3ddb76db62d3b9b444c67a3bc6cc6e2da8b9c73e
Sander Tuit <sandertuit@gmail.com>
2013-12-13 13:15:31 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ LICENSE = ''
 
 setup(
     name = 'django-tapatalk',
-    version = '0.5.8.27',
+    version = '0.5.8.28',
     description='DjangoBB Tapatalk implementation',
     long_description=README,
     author = 'Sander van de Graaf',
