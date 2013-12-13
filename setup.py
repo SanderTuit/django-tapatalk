ff049eec1bb9e46ad389c648b1de85c0e2bc6e2b
Sander Tuit <sandertuit@gmail.com>
2013-12-13 17:02:26 +0100

    Revert changes to short_content

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ LICENSE = ''
 
 setup(
     name = 'django-tapatalk',
-    version = '0.5.8.35',
+    version = '0.5.8.36',
     description='DjangoBB Tapatalk implementation',
     long_description=README,
     author = 'Sander van de Graaf',
