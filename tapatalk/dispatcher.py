d38a252d309ccba4f989e7f3288b208fd9292136
Sander Tuit <sandertuit@gmail.com>
2014-12-15 10:15:25 +0100

    Debugging all the methods

--- tapatalk/dispatcher.py
+++ tapatalk/dispatcher.py
@@ -5,21 +5,42 @@ from tapatalk import settings
 # If we need to debug, now we know
 DEBUG = hasattr(settings, 'XMLRPC_DEBUG') and settings.XMLRPC_DEBUG
 
+from django.core.mail import EmailMultiAlternatives
+from django.core.mail import send_mail
+def send_mail(subject, text, from_email, rec_list, html=None):
+    """
+    Shortcut for sending email.
+    """
+    msg = EmailMultiAlternatives(subject, text, from_email, rec_list)
+    if html:
+        msg.attach_alternative(html, "text/html")
+    if settings.DEBUG:
+        print '---begin---'
+        print 'To:', rec_list
+        print 'Subject:', subject
+        print 'Body:', text
+        print '---end---'
+    msg.send(fail_silently=True)
 
 class TapatalkXMLRPCDispatcher(DjangoXMLRPCDispatcher):
     def _marshaled_dispatch(self, request, django_args, django_kwargs):
         def dispatch_method(method, params):
             try:
-                func = self.funcs[method]
-            except KeyError:
-                raise Exception('method "%s" is not supported' % method)
+                try:
+                    func = self.funcs[method]
+                except KeyError:
+                    raise Exception('method "%s" is not supported' % method)
 
-            kwargs = {}
-            if django_args:
-                kwargs['django_args'] = django_args
-            if django_kwargs:
-                kwargs['django_kwargs'] = django_kwargs
-            return func(request, *params, **kwargs)
+                kwargs = {}
+                if django_args:
+                    kwargs['django_args'] = django_args
+                if django_kwargs:
+                    kwargs['django_kwargs'] = django_kwargs
+
+                return func(request, *params, **kwargs)
+            except:
+                send_mail("Debugging Forum", str(django_args) + " " + str(django_kwargs), settings.DEFAULT_FROM_EMAIL, ["sander@androidworld.nl"])
+
 
         # Tapatalk sends out bad formatted booleans... *sigh*
         body = request.raw_post_data.replace('<boolean>true</boolean>', '<boolean>1</boolean>')
--- tapatalk/subscription.py
+++ tapatalk/subscription.py
@@ -1,40 +1,18 @@
 from util import *
-import sys
 
-from django.core.mail import EmailMultiAlternatives
-from django.core.mail import send_mail
-def send_mail(subject, text, from_email, rec_list, html=None):
-    """
-    Shortcut for sending email.
-    """
-    msg = EmailMultiAlternatives(subject, text, from_email, rec_list)
-    if html:
-        msg.attach_alternative(html, "text/html")
-    if settings.DEBUG:
-        print '---begin---'
-        print 'To:', rec_list
-        print 'Subject:', subject
-        print 'Body:', text
-        print '---end---'
-    msg.send(fail_silently=True)
+
 
 
 def get_subscribed_topic(request, start_num=0, last_num=20):
+    
+    topics = request.user.subscriptions.all()
+
     data = {
-            'total_topic_num': 0,
-            'topics': []
-        }
-    try:
-        topics = request.user.subscriptions.all()
+        'total_topic_num': len(topics),
+        'topics': []
+    }
 
-        data = {
-            'total_topic_num': len(topics),
-            'topics': []
-        }
-
-        for topic in topics:
-            data['topics'].append(topic.as_tapatalk())
-    except:
-        send_mail("Debugging Forum", sys.exc_info()[0], settings.DEFAULT_FROM_EMAIL, ["sander@androidworld.nl"])
+    for topic in topics:
+        data['topics'].append(topic.as_tapatalk())
 
     return data
